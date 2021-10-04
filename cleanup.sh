a6346b12e03a24006589b48e453b85944d0e863f
Deyan Dichev <dejan1238@aim.com>
2021-10-04 09:46:12 -0400

    add cleanup and provision script add templates add snmp plugin

--- cleanup.sh
+++ cleanup.sh
@@ -2,3 +2,7 @@
 
 docker-compose down
 docker system prune --volumes --force
+rm grafana/dashboards/XCVR-Status.json
+rm grafana/dashboards/SAOS10xTx_Stats_PortStatus.json
+rm grafana/provisioning/datasources/influxdb.yml
+rm telegraf/etc/telegraf.conf
