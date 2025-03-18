3a2edee9465069df55f02c2d48382654fc4232c5
Deyan Dichev <dejan1238@aim.com>
2025-03-18 12:51:12 +0000

    clean volumes and images

--- cleanup.sh
+++ cleanup.sh
@@ -2,7 +2,9 @@
 
 docker-compose down
 docker system prune --volumes --force
-rm grafana/dashboards/XCVR-Status.json
-rm grafana/dashboards/SAOS10xTx_Stats_PortStatus.json
+docker rmi `sudo docker image ls | egrep telegraf\|grafana\|influxdb | awk '{print$3}'`
+docker volume rm `sudo docker volume ls | egrep network-monitoring | awk '{print$2}'`
+#rm grafana/dashboards/XCVR-Status.json
+#rm grafana/dashboards/SAOS10xTx_Stats_PortStatus.json
 rm grafana/provisioning/datasources/influxdb.yml
 rm telegraf/etc/telegraf.conf
